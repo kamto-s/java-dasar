ef1a5fe6c0bbed41c03cfa4d7fd8a4ba23851545
kamto-s <kamto.076s@gmail.com>
2025-10-05 02:27:19 +0700

    first commit

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,22 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 123;
+        short iniShort = 12345;
+        int iniInt = 1234567890;
+        long iniLong = 1234567890;
+        long iniLong2 = 1234567890L;
+
+//        floating point number
+        float iniFloat = 12.12F;
+        double iniDouble = 12.1234567;
+
+//        Literals
+        int decimalInt = 25;
+        int hexaDecimal = 0xFFFFFF;
+        int binaryDecimal = 0b101010;
+
+//        underscore
+        long balance = 1_000_000_000;
+        
+    }
+}
